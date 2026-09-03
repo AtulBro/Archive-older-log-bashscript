31ca58801250c007f3bd5254bfc2e9eeaa966f4c
Super User <root@localhost.localdomain>
2026-09-03 12:04:40 +0530

    v1.0

---
+++ Archive-Older-log/archive-project.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#===============================
+#Date:$date$
+#Version: v1.0
+#===============================
+
+#Variables
+
+BASE="/root/Documents/devops-learning/shell-scripting/projects/folder"
+DAYS=10
+DEPTH=1 #File will only search for that folder only will not check other folder
+RUN=0
+
+#Check if the directory is present or not
+
+if [[ ! -d $BASE ]]
+then
+	echo "Directory does not exist: $BASE"
+	exit 1
+fi
+
+#Create 'archive' folder is present or not
+if [[ ! -d $BASE/archive ]] 
+then 
+	mkdir $BASE/archive
+fi
+
+#Find the list of file larger than 12mb
+
+for i in `find $BASE -maxdepth $DEPTH -type f -size +12M`
+do
+	if [[ $RUN -eq 0 ]] 
+	then
+		echo "[$(date "+%Y-%m-%d %H:%M:%S")] archiving $i ==> $BASE/archive"
+		gzip $i || exit 1
+		mv "$i.gz" "$BASE/archive" || exit 1
+	fi
+done
+
